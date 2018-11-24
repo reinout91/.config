1f88477ed1aab1b48f74a52cca614fab1d30d218
reinout <reinout91@gmail.com>
2018-11-24 18:33:32 +0100

    test

--- fish/functions/syncall.fish
+++ fish/functions/syncall.fish
@@ -1,1 +1,10 @@
-function syncall ()
+function syncall --argument message
+	set folders ~\.config
+	echo $folders
+	git config --global credential.helper "cache --timeout 7200"
+	for i in $folders
+		git add .
+		git commit -m $message
+		git push
+	end
+end
