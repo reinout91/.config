023ad762081d0e93da6c6a18041a185d8091d6a8
Reinout <reinout91@gmail.com>
2019-01-08 00:48:13 +0100

    huy

---
+++ fish/functions/ccat.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function ccat --description 'alias ccat=pygmentize -g'
+	pygmentize -g $argv;
+end
---
+++ fish/functions/cdv.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function cdv --description 'alias cdv=cd ~/projects/vue/prophesy/src'
+	cd ~/projects/vue/prophesy/src $argv;
+end
